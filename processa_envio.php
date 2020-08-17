711e72b16716bb2634d03286e5386a7c494798a8
xJocz <49736229+xJocz@users.noreply.github.com>
2020-08-17 10:06:59 -0300

    Update processa_envio.php
    Remoção da conta gmail para evitar acessos de terceiros.

--- processa_envio.php
+++ processa_envio.php
@@ -46,8 +46,8 @@
             $mail->isSMTP();                                          // Set mailer to use SMTP
             $mail->Host = 'smtp.gmail.com';                           // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                                   // Enable SMTP authentication
-            $mail->Username = 'joczgabriel90@gmail.com';              // SMTP username
-            $mail->Password = 'barracuda@00';                         // SMTP password
+            $mail->Username = '';              // SMTP username
+            $mail->Password = '';                         // SMTP password
             $mail->SMTPSecure = 'tls';                                // Enable TLS encryption, `ssl` also accepted
             $mail->Port = 587;                                        // TCP port to connect to
 
